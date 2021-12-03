da7fbcbc6e150a72c43e94a58c50f6b28c736588
haloflooder <haloflooder@gmail.com>
2021-12-03 12:02:38 -0500

    Forgot about part 2

--- 2021/day01/solution.bat
+++ 2021/day01/solution.bat
@@ -2,24 +2,77 @@
 rem Lets make the colors festive-y
 color 24
 
-rem This allows us to use variables in a for loop. I don't know WHY this is. Apparentely because the entire for loop is a single command supposedly.
-setlocal EnableDelayedExpansion
-
 set counter=0
-set prevDepth=-1
+goto :main
 
-for /f "tokens=*" %%d in (input.txt) do (
-    if %%d GTR !prevDepth! (
+:findIndex
+
+set "index=0"
+
+:findIndexLoop
+if defined data[%index%] (
+    set /a "index+=1"
+    goto :findIndexLoop
+)
+
+exit /b 0
+
+:processDepth
+set prevDepth=-1
+set index=0
+call :findIndex
+
+set /a "index-=1"
+for /l %%i in (0,1,%index%) do (
+    set /a depth=data[%%i]
+    if !depth! GTR !prevDepth! (
         if !prevDepth! EQU -1 (
-            echo "%%d (N/A)"
+            echo "!depth! (N/A)"
         ) else (
-            set /a counter=!counter!+1
-            echo %%d (increased^)
+            set /a "counter+=1"
+            echo !depth! (increased^)
         )
     ) else (
-        echo %%d (decreased^)
+        echo !depth! (decreased^)
     )
     
-    set prevDepth=%%d
+    set prevDepth=!depth!
 )
+
+exit /b 0
+
+:main
+rem This allows us to process variables in "realtime" in for loops
+setlocal EnableDelayedExpansion
+set counter=0
+set data=
+set depthData=
+set index=0
+for /f "tokens=*" %%d in (input.txt) do (
+    set data[!index!]=%%d
+    set depthData[!index!]=%%d
+    set /a index=!index!+1
+)
+call :processDepth
+
 echo The depth increased %counter% times
+
+set index=0
+call :findIndex
+
+set data=
+set /a "index-=2"
+for /l %%i in (0,1,%index%) do (
+    set /a d1=%%i
+    set /a d2=%%i+1
+    set /a d3=%%i+2
+    echo !d1! !d2! !d3!
+    set /a "depth=depthData[!d1!]+depthData[!d2!]+depthData[!d3!]"
+    echo !depth!
+    set /a data[%%i]=!depth!
+)
+
+set counter=0
+call :processDepth
+
+echo The 3 sum depth increased %counter% times
